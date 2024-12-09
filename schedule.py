0f1e02233275b143cb67af2565d336bc2ea1f80f
jiji-krk <152401127+jiji-krk@users.noreply.github.com>
2024-12-09 09:27:58 +0100

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -27,7 +27,8 @@ def scrape_flights():
         "--ignore-certificate-errors",
         "--disable-extensions",
         "--no-sandbox",
-        "--disable-dev-shm-usage"
+        "--disable-dev-shm-usage",
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     ]
     for option in options:
         chrome_options.add_argument(option)
